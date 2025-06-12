5707bfcfa1013ebc0595f299f6820ed7ddf47c7f
Shajibmia96 <shajibahamedornob@gmail.com>
2025-06-12 16:50:28 +0600

    01_variable added

---
+++ 01_variable.js
@@ -0,0 +1,15 @@
+// now we know that we can use variables.
+
+var myName = "John";
+let myAge = 30;
+const myCountry = "USA";
+// Variables can be reassigned if they are declared with var or let, but not with const.
+
+console.table([myName, myAge, myCountry]);
+
+// * peffer not use var, use let or const instead
+// * const is used for variables that should not change, like configuration values or constants.
+
+let naeme = "John";
+
+console.log(naeme);
