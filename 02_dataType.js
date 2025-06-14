8ef65808023cda29f3e750ae243803bccb6427b8
Shajibmia96 <shajibahamedornob@gmail.com>
2025-06-14 10:38:21 +0600

    we discuse about datatypes

---
+++ 02_dataType.js
@@ -0,0 +1,23 @@
+
+// number = 2 power of 53
+/*
+   string = "John Doe"
+   number = 42
+   boolean = true\false
+   bigint = 9007199254740991n
+   undefined = undefined
+   null = null\ stanaleone value
+   symbol = Symbol('description') || unique indentifier
+
+
+   object = { key: 'value' } || [1, 2, 3]
+
+  */
+
+const naem = "John Doe";
+const number = 42;
+let isActive = true;
+console.log(typeof number, typeof naem, typeof isActive);
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
