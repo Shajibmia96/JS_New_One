1bae84b3d9d1c1f97a0b9fbf1dfc7eaff7ab2e06
Shajibmia96 <shajibahamedornob@gmail.com>
2025-06-14 12:53:18 +0600

    added conversion of number, string, boolean

---
+++ 03_convertionOfOperator.js
@@ -0,0 +1,32 @@
+let scrore ='33a';
+
+console.log(scrore); // 33
+
+let scroreNumber = Number(scrore); // convert string to number
+console.log(typeof scrore); // number
+console.log(scroreNumber); // 33
+console.log(typeof scroreNumber); // number
+
+// "33" => 33
+// "33a" => NaN
+//true => 1
+// false => 0
+// null => 0
+// undefined => NaN   
+
+let isActive = ' eew';
+let isActiveNumber = Boolean(isActive); // convert boolean to number
+
+console.log(isActiveNumber); // true
+console.log(typeof isActiveNumber); // boolean
+
+// isActive = 1 => true
+// isActive = 0 => false    
+// isActive = ' ' => true
+// isActive = 'shajib' => true
+// isActive = '' => false 
+// isActive = null => false
+// isActive = undefined => false  
+// isActive = NaN => false
+
+
