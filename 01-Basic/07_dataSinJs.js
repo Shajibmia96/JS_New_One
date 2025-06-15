c0e8ac5b78ea2ab307bba6ba17eaa4420ef6117d
Shajibmia96 <shajibahamedornob@gmail.com>
2025-06-15 16:25:34 +0600

    now we explore date

---
+++ 01-Basic/07_dataSinJs.js
@@ -0,0 +1,33 @@
+const myDate = new Date().toString();
+const myDates = new Date().toDateString();
+const myDatess = new Date().toTimeString();
+const myDatesss = new Date().toJSON();
+const myDatessss = new Date().toLocaleString();
+
+// console.log(myDate);
+// console.log(myDates);
+// console.log(myDatess);
+// console.log(myDatesss);
+// console.log(myDatessss);
+
+
+const myCreatedDate = new Date(2002, 9, 25, 2, 6 );
+
+// console.log(myCreatedDate.toLocaleString());
+// console.log(myCreatedDate.toLocaleTimeString());
+
+
+const dateTepms = Date.now();
+
+console.log(dateTepms);
+
+console.log(Math.floor((Date.now())/1000));
+ 
+const newDate = new Date();
+
+console.log(newDate.toLocaleString(""));
+
+
+
+
+
