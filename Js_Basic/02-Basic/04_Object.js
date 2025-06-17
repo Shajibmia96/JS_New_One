71a2b6d1f6e2bdeef7bfbbbe6381b09930a9fd50
Shajibmia96 <shajibahamedornob@gmail.com>
2025-06-17 09:43:22 +0600

    Object methods done

--- Js_Basic/02-Basic/04_Object.js
+++ Js_Basic/02-Basic/04_Object.js
@@ -7,4 +7,38 @@ tingerUser.id = 15700;
 tingerUser.name = "Shajib Mia";
 tingerUser.address = "Araihazar, Narayangonj";
 
-console.log(tingerUser);
+// console.log(tingerUser);
+
+//  console.log(Object.keys(tingerUser));
+//  console.log(Object.values(tingerUser));
+//  console.log(Object.entries(tingerUser));
+ console.log(tingerUser.hasOwnProperty('names'));
+ 
+ 
+const regularUser = {
+    email : "shajib@gmail.com",
+    userfullName :{
+         userName : {
+            firstName : {
+                name: "Shajib"
+            }
+         }
+    }
+}
+
+// console.log(regularUser?.userfullName?.userName?.firstName?.name);
+// with optional chailing 
+
+
+
+  const obj1 = {1 : "b", 2:"a"};
+
+  const obj2 = {4:'B', 5:"6"}
+ 
+  const obj4= Object.assign({} ,obj1, obj2)
+  const obj3 = {...obj1, ...obj2}
+  //  console.log(obj4);
+   
+  // console.log(obj3);
+  
+
