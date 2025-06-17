b48abd5b40fcaf78b18055fa3bdeb894a1ad9184
Shajibmia96 <shajibahamedornob@gmail.com>
2025-06-17 09:08:20 +0600

    Expore js Object

---
+++ Js_Basic/02-Basic/04_Object.js
@@ -0,0 +1,10 @@
+
+// const tingerUser = new Object()
+
+const tingerUser = {};
+
+tingerUser.id = 15700;
+tingerUser.name = "Shajib Mia";
+tingerUser.address = "Araihazar, Narayangonj";
+
+console.log(tingerUser);
