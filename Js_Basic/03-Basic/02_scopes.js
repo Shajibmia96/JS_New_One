8d7d47938a6d2bb8ba1094f032b7a309ae58b027
Shajibmia96 <shajibahamedornob@gmail.com>
2025-06-19 11:54:14 +0600

    here explore about scope

---
+++ Js_Basic/03-Basic/02_scopes.js
@@ -0,0 +1,16 @@
+
+
+let number = 25;
+
+console.log(number);
+
+
+if(true){
+
+     const number = 40;
+     console.log(number ," this print for block scope");
+     
+}
+
+console.log(number, "this is print for global scope");
+
