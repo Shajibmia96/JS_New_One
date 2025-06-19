de75b35b702cf3c8e3a4e9cc39021f0b55d2f494
Shajibmia96 <shajibahamedornob@gmail.com>
2025-06-19 12:13:10 +0600

    here explore about nested  scope

--- Js_Basic/03-Basic/02_scopes.js
+++ Js_Basic/03-Basic/02_scopes.js
@@ -2,15 +2,35 @@
 
 let number = 25;
 
-console.log(number);
+//console.log(number);
 
 
 if(true){
 
      const number = 40;
-     console.log(number ," this print for block scope");
+    // console.log(number ," this print for block scope");
      
 }
 
-console.log(number, "this is print for global scope");
+// console.log(number, "this is print for global scope");
+
+
+
+function one () {
+      const userName = "Shajib Mia";
+
+       function two(){
+          const  website = "www.shajib.com";
+          console.log(userName);
+          
+       }
+        
+        //  console.log(website);
+         
+         two()
+
+}
+
+one()
+
 
